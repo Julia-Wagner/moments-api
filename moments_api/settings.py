aa2b0a86b3cdf33a7f32b58548888f012c15e821
julia Wager <julia@p-wagner.at>
2024-02-28 21:08:59 +0100

    create profiles app

--- moments_api/settings.py
+++ moments_api/settings.py
@@ -47,9 +47,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
