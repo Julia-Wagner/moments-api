ea64f908fa0de2256f267bac952b6b2afc4b2b40
julia Wager <julia@p-wagner.at>
2024-03-01 17:32:52 +0100

    add authentication and permissions

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,6 +4,10 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+
 
     class Meta:
         model = Profile
