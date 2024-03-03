71a7db18b91fcb80acac5686daa58e0f58924807
julia Wager <julia@p-wagner.at>
2024-03-03 08:58:19 +0100

    update CORS settings

--- moments_api/settings.py
+++ moments_api/settings.py
@@ -111,9 +111,15 @@ if 'CLIENT_ORIGIN' in os.environ:
         os.environ.get('CLIENT_ORIGIN')
     ]
 else:
+    CORS_ALLOWED_ORIGINS = []
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^:8000$",
+        r"^192.168.1.15:3000$",
     ]
+
+# Add local development origin if CLIENT_ORIGIN_DEV is set
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    dev_origin = os.environ.get('CLIENT_ORIGIN_DEV')
+    CORS_ALLOWED_ORIGINS.append(dev_origin)
 
 CORS_ALLOW_CREDENTIALS = True
 
